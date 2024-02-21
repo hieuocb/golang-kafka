3de48c15f2cadfdc11d9c395035ced610a67d2d7
noka <hieunt27@ocb.com.vn>
2024-02-21 11:04:18 +0700

    update

--- consumer.go
+++ consumer.go
@@ -11,17 +11,12 @@ func main() {
 	// Set up Kafka consumer configuration
 
 	config := &kafka.ConfigMap{
-
-		"bootstrap.servers": "localhost:9092", // broker addr
-
-		"group.id": "console-consumer-21564", // consumer group
-
-		"auto.offset.reset": "earliest", // earliest offset
-
+		"bootstrap.servers": "localhost:9092",         // broker addr
+		"group.id":          "console-consumer-21564", // consumer group
+		"auto.offset.reset": "earliest",               // earliest offset
 	}
 
 	// Create Kafka consumer
-
 	consumer, err := kafka.NewConsumer(config)
 
 	if err != nil {
@@ -31,29 +26,20 @@ func main() {
 	}
 
 	// subscribe to target topic
-
 	consumer.SubscribeTopics([]string{"coordinates"}, nil)
 
 	// Consume messages
-
 	for {
-
 		msg, err := consumer.ReadMessage(-1)
 
 		if err == nil {
-
 			fmt.Printf("Received message: %s\n", string(msg.Value))
-
 		} else {
-
 			fmt.Printf("Error while consuming message: %v (%v)\n", err, msg)
-
 		}
-
 	}
 
 	// Close Kafka consumer
-
 	consumer.Close()
 
 }
